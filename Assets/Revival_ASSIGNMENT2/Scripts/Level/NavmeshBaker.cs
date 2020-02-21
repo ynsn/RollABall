9beb9ed48fa119c1cc475f1e03ad045bd99c39a9
Yoram Janssen <yoramjanssenn@gmail.com>
2020-02-21 14:56:12 +0100

    YES

---
+++ Assets/Revival_ASSIGNMENT2/Scripts/Level/NavmeshBaker.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(NavMeshSurface))]
+public class NavmeshBaker : MonoBehaviour
+{
+    [SerializeField] private NavMeshSurface surface;
+
+    [SerializeField] private List<GameObject> platformObjects;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject o = GameObject.FindObjectsOfType<NavMeshSurface>()
+        
+        surface = GetComponent<NavMeshSurface>();
+        surface.BuildNavMesh();
+    }
+
+    public void UpdateNavigationMesh()
+    {
+        surface.BuildNavMesh();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Slash))
+        {
+            surface.BuildNavMesh();
+        }
+    }
+}
--- Assets/Revival_ASSIGNMENT2/Scripts/Player/PlayerController.cs
+++ Assets/Revival_ASSIGNMENT2/Scripts/Player/PlayerController.cs
@@ -1,13 +1,48 @@
 ﻿using System;
+using System.Numerics;
 using UnityEngine;
+using UnityEngine.AI;
+using Vector3 = UnityEngine.Vector3;
 
+[RequireComponent(typeof(NavMeshAgent))]
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private string platformTag = "Platform";
     [SerializeField] private Camera playerCamera;
 
+    [SerializeField] private int whipReach = 1;
+    [SerializeField] private float speed = 1.0f;
+
+    private NavMeshAgent agent;
+
+    public delegate void PlayerMove(Vector3 source, Vector3 target);
+
+    public event PlayerMove OnPlayerMove;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
     private void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+
+            Ray ray = playerCamera.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Transform objectHit = hit.transform;
+
+                if (objectHit.GetComponent<NavMeshSurface>())
+                {
+                    Debug.Log("Moving player...");
+                    OnPlayerMove(transform.position, hit.point);
+                    agent.destination = hit.point;
+                }
+            }
+        }
     }
 }
