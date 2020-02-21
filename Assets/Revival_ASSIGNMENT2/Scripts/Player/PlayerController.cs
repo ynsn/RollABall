a5779c8be9b6d302a84cf52650eb9b9fbe26d07f
Yoram Janssen <yoramjanssenn@gmail.com>
2020-02-21 14:16:54 +0100

    Player controller script

---
+++ Assets/Revival_ASSIGNMENT2/Scripts/Player/PlayerController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private string platformTag = "Platform";
+    [SerializeField] private Camera playerCamera;
+
+    private void Update()
+    {
+        
+    }
+}
