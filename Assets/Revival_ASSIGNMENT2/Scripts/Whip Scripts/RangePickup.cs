1b87a71c5680fa2343aef353a101ae2c4a4af3eb
MonikaSimma <msi7266@students.fhv.at>
2020-02-21 15:35:28 +0100

    Some shit

--- Assets/Revival_ASSIGNMENT2/Scripts/Whip Scripts/RangePickup.cs
+++ Assets/Revival_ASSIGNMENT2/Scripts/Whip Scripts/RangePickup.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 [RequireComponent(typeof(Collider))] //whatever the game object must have a collider
 public class RangePickup : MonoBehaviour
 {
+    [SerializeField] private float increaseValueBy = 1f;
+
     private void OnTriggerEnter(Collider other) //anytime smth interacts with this, it will be fired
     {
-        if (other.GetComponent<PlayerController>() != null) //looking for the player component and see if it exists
+        if (other.GetComponent<WhipBehavior>() != null) //looking for the player component and see if it exists
         {
+            other.GetComponent<WhipBehavior>().IncreaseRange(increaseValueBy);
             Destroy(gameObject);
         }
     }
---
+++ Assets/Revival_ASSIGNMENT2/Scripts/Whip Scripts/WhipBehavior.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhipBehavior : MonoBehaviour
+{
+    private NavmeshBaker navBaker = null;
+
+    [SerializeField] private float range = 5f;
+
+    private void Start()
+    {
+        navBaker = FindObjectOfType<NavmeshBaker>();
+        navBaker.UpdateNavigationMesh();
+    }
+
+    public void CheckIfInRange(Vector3 source, Vector3 target)
+    {
+
+    }
+
+    /// <summary> Increases range of this whip. </summary>
+    /// <param name="value"> Value to increase range by. </param>
+    public void IncreaseRange(float value)
+    {
+        range += value;
+    }
+}
